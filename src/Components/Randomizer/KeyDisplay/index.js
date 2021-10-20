3c0a122f798d987e676d75b6e3a98eaa06888e03
Luke <Lukem>
2021-10-20 18:03:24 +1100

    successfully refactor keydisplay

---
+++ src/Components/Randomizer/KeyDisplay/index.js
@@ -0,0 +1,134 @@
+import {Howler} from 'howler';
+import Countdown from 'react-countdown';
+import { Paper, Grid, Button } from '@mui/material';
+import {chords} from "../../../utils/musicImports";
+import {generateRandomKey, generateSequentialKey, generateFifthsKey} from "../../../utils/keyChanges";
+import {useState} from 'react';
+
+const KeyDisplay = props => {
+    const {keyOrder, setKeyOrder, modifiers, styles, delayInSeconds} = props;
+    const keysWithFlats = ["Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G"];
+    // const keysWithSharps = ["A","A#","B","C","C#","D","D#","E","F","F#","G", "G#"];
+  
+    const [currentKeyset, setCurrentKeyset] = useState(keysWithFlats);
+  
+    const [currentKey, setCurrentKey] = useState("C");
+    const [upcomingKey,setUpcomingKey] = useState("Db");
+    const [currentMod, setCurrentMod] = useState("Major");
+    const [upcomingMod,setUpcomingMod] = useState("Major");
+
+
+    // const randomizeModIfEnabled = () => {
+    //   if (randomizeMod) {
+    //     let newMod = modifiers[Math.floor(Math.random() * modifiers.length)];
+    //     setUpcomingMod(newMod);
+    //   }
+    // }
+
+    const decideUpcomingKey = () => {
+      switch (keyOrder) {
+        case "randomKey": 
+          return (generateRandomKey(currentKeyset));
+        case "sequential":
+          // We pass in upcoming so it starts from correct key
+          return (generateSequentialKey(upcomingKey, currentKeyset));
+          case "fifths": 
+          return (generateFifthsKey(upcomingKey, currentKeyset));
+        default: {
+          break;
+        }
+      }
+    } 
+    const playChord = () => {
+      let chord = chords[`${upcomingKey}${upcomingMod}`];
+      chord.play()
+      if (chord) {
+
+        // chord.once('load', () => {})
+        // chord.volume = volume / 100;
+        // chord.play();
+        // // TODO - if loop selected
+        // chord.loop = loop;
+      }
+      else {
+        console.error('no chord found')
+      }
+    }
+  
+
+    const clockRenderer = ({ hours, minutes, seconds, completed, api}) => {
+
+      // TODO - when 3 sec left, play click (but dont alter state)
+      if (completed) {
+
+        // start: current = G. Upcoming = Ab.
+        // Countdown begins.
+        // upcon complettion..
+
+        // upcoming = Ab (same as before)
+        
+        // Stop any previous chords
+        Howler.stop()
+        // Play current
+        playChord();
+        
+        //Restart clock
+        api.start();
+        
+        // Current is discarded. 
+        // Upcoming becomes current.  
+        
+        setUpcomingKey(decideUpcomingKey());
+        setCurrentKey(upcomingKey);
+
+        // Change state for next reset
+
+        // Always randomly select from list of available mods (chosen by user)
+        setUpcomingMod(modifiers[Math.floor(Math.random() * modifiers.length)]);
+        setCurrentMod(upcomingMod);
+
+
+        return <h1>paused</h1>
+        } 
+        
+        else {
+          // Render a countdown
+          return (
+            <div style={styles.countdown} >
+            
+            <span>{(hours > 0) && hours} {minutes} mins {seconds} sec</span>
+            </div>
+          )
+        }
+    };
+
+
+   
+    return (
+<>
+      <Paper elevation={4}>
+        <Grid container style={styles.keyDisplayGrid}>
+          <Grid item xs={6} style={styles.keyDisplayItem}>
+            <h5 style={styles.gridItemText}>Current:</h5> 
+            <h5 style={styles.gridItemText}>{currentKey} {currentMod}</h5> 
+
+          </Grid>
+          <Grid item xs={6} style={styles.keyDisplayItem}>  
+                <h5 style={styles.gridItemText}>Next:</h5> 
+                <h5 style={styles.gridItemText}>{upcomingKey} {upcomingMod}</h5> 
+          </Grid>
+         </Grid> 
+
+      </Paper>
+
+    <Countdown
+        date={Date.now() + delayInSeconds * 1000}
+        renderer={clockRenderer}
+    />
+</>
+
+      
+    )
+  }
+
+export default KeyDisplay;
--- src/Components/Randomizer/index.js
+++ src/Components/Randomizer/index.js
@@ -1,9 +1,7 @@
 import {useState} from 'react';
-import {Howler} from 'howler';
-import Countdown from 'react-countdown';
-import {chords} from "../../utils/musicImports";
+import KeyDisplay from "./KeyDisplay";
 import {generateRandomKey, generateSequentialKey, generateFifthsKey} from "../../utils/keyChanges";
-import { Paper, Grid, Slide, TextField, Button } from '@mui/material';
+import { Paper, Grid, TextField, Button } from '@mui/material';
 
 
 
@@ -87,125 +85,125 @@ const Randomizer = () => {
     // }
   }
 
-  const KeyDisplay = () => {
-    const [currentKey, setCurrentKey] = useState("C");
-    const [upcomingKey,setUpcomingKey] = useState("Db");
-    const [currentMod, setCurrentMod] = useState("Major");
-    const [upcomingMod,setUpcomingMod] = useState("Major");
+//   const KeyDisplay = () => {
+//     const [currentKey, setCurrentKey] = useState("C");
+//     const [upcomingKey,setUpcomingKey] = useState("Db");
+//     const [currentMod, setCurrentMod] = useState("Major");
+//     const [upcomingMod,setUpcomingMod] = useState("Major");
 
 
-    // const randomizeModIfEnabled = () => {
-    //   if (randomizeMod) {
-    //     let newMod = modifiers[Math.floor(Math.random() * modifiers.length)];
-    //     setUpcomingMod(newMod);
-    //   }
-    // }
+//     // const randomizeModIfEnabled = () => {
+//     //   if (randomizeMod) {
+//     //     let newMod = modifiers[Math.floor(Math.random() * modifiers.length)];
+//     //     setUpcomingMod(newMod);
+//     //   }
+//     // }
 
-    const decideUpcomingKey = () => {
-      switch (keyOrder) {
-        case "randomKey": 
-          return (generateRandomKey(currentKeyset));
-        case "sequential":
-          // We pass in upcoming so it starts from correct key
-          return (generateSequentialKey(upcomingKey, currentKeyset));
-          case "fifths": 
-          return (generateFifthsKey(upcomingKey, currentKeyset));
-        default: {
-          break;
-        }
-      }
-    } 
-    const playChord = () => {
-      let chord = chords[`${upcomingKey}${upcomingMod}`];
-      chord.play()
-      if (chord) {
+//     const decideUpcomingKey = () => {
+//       switch (keyOrder) {
+//         case "randomKey": 
+//           return (generateRandomKey(currentKeyset));
+//         case "sequential":
+//           // We pass in upcoming so it starts from correct key
+//           return (generateSequentialKey(upcomingKey, currentKeyset));
+//           case "fifths": 
+//           return (generateFifthsKey(upcomingKey, currentKeyset));
+//         default: {
+//           break;
+//         }
+//       }
+//     } 
+//     const playChord = () => {
+//       let chord = chords[`${upcomingKey}${upcomingMod}`];
+//       chord.play()
+//       if (chord) {
 
-        // chord.once('load', () => {})
-        // chord.volume = volume / 100;
-        // chord.play();
-        // // TODO - if loop selected
-        // chord.loop = loop;
-      }
-      else {
-        console.error('no chord found')
-      }
-    }
+//         // chord.once('load', () => {})
+//         // chord.volume = volume / 100;
+//         // chord.play();
+//         // // TODO - if loop selected
+//         // chord.loop = loop;
+//       }
+//       else {
+//         console.error('no chord found')
+//       }
+//     }
   
 
-    const clockRenderer = ({ hours, minutes, seconds, completed, api}) => {
+//     const clockRenderer = ({ hours, minutes, seconds, completed, api}) => {
 
-      // TODO - when 3 sec left, play click (but dont alter state)
-      if (completed) {
+//       // TODO - when 3 sec left, play click (but dont alter state)
+//       if (completed) {
 
-        // start: current = G. Upcoming = Ab.
-        // Countdown begins.
-        // upcon complettion..
+//         // start: current = G. Upcoming = Ab.
+//         // Countdown begins.
+//         // upcon complettion..
 
-        // upcoming = Ab (same as before)
+//         // upcoming = Ab (same as before)
         
-        // Stop any previous chords
-        Howler.stop()
-        // Play current
-        playChord();
+//         // Stop any previous chords
+//         Howler.stop()
+//         // Play current
+//         playChord();
         
-        //Restart clock
-        api.start();
+//         //Restart clock
+//         api.start();
         
-        // Current is discarded. 
-        // Upcoming becomes current.  
+//         // Current is discarded. 
+//         // Upcoming becomes current.  
         
-        setUpcomingKey(decideUpcomingKey());
-        setCurrentKey(upcomingKey);
+//         setUpcomingKey(decideUpcomingKey());
+//         setCurrentKey(upcomingKey);
 
-        // Change state for next reset
+//         // Change state for next reset
 
+//         // Always randomly select from list of available mods (chosen by user)
+//         setUpcomingMod(modifiers[Math.floor(Math.random() * modifiers.length)]);
+//         setCurrentMod(upcomingMod);
+
+
+//         return <h1>paused</h1>
+//         } 
         
-        setUpcomingMod(modifiers[Math.floor(Math.random() * modifiers.length)]);
-        setCurrentMod(upcomingMod);
-
-
-        return <h1>paused</h1>
-        } 
-        
-        else {
-          // Render a countdown
-          return (
-            <div style={styles.countdown} >
+//         else {
+//           // Render a countdown
+//           return (
+//             <div style={styles.countdown} >
             
-            <span>{(hours > 0) && hours} {minutes} mins {seconds} sec</span>
-            </div>
-          )
-        }
-    };
+//             <span>{(hours > 0) && hours} {minutes} mins {seconds} sec</span>
+//             </div>
+//           )
+//         }
+//     };
 
 
    
-    return (
-<>
-      <Paper elevation={4}>
-        <Grid container style={styles.keyDisplayGrid}>
-          <Grid item xs={6} style={styles.keyDisplayItem}>
-            <h5 style={styles.gridItemText}>Current:</h5> 
-            <h5 style={styles.gridItemText}>{currentKey} {currentMod}</h5> 
+//     return (
+// <>
+//       <Paper elevation={4}>
+//         <Grid container style={styles.keyDisplayGrid}>
+//           <Grid item xs={6} style={styles.keyDisplayItem}>
+//             <h5 style={styles.gridItemText}>Current:</h5> 
+//             <h5 style={styles.gridItemText}>{currentKey} {currentMod}</h5> 
 
-          </Grid>
-          <Grid item xs={6} style={styles.keyDisplayItem}>  
-                <h5 style={styles.gridItemText}>Next:</h5> 
-                <h5 style={styles.gridItemText}>{upcomingKey} {upcomingMod}</h5> 
-          </Grid>
-         </Grid> 
+//           </Grid>
+//           <Grid item xs={6} style={styles.keyDisplayItem}>  
+//                 <h5 style={styles.gridItemText}>Next:</h5> 
+//                 <h5 style={styles.gridItemText}>{upcomingKey} {upcomingMod}</h5> 
+//           </Grid>
+//          </Grid> 
 
-      </Paper>
+//       </Paper>
 
-    <Countdown
-        date={Date.now() + delayInSeconds * 1000}
-        renderer={clockRenderer}
-    />
-</>
+//     <Countdown
+//         date={Date.now() + delayInSeconds * 1000}
+//         renderer={clockRenderer}
+//     />
+// </>
 
       
-    )
-  }
+//     )
+//   }
 
   const ChangeOrderDisplay = () => {
     return (
@@ -290,7 +288,13 @@ const Randomizer = () => {
     <Paper elevation={5} style={styles.paperContainer}>
      <h1>Key Randomizer</h1> 
       <div>
-        <KeyDisplay />
+        <KeyDisplay 
+          keyOrder={keyOrder}
+          setKeyOrder={setKeyOrder}
+          modifiers={modifiers}
+          styles={styles}
+          delayInSeconds={delayInSeconds}
+        />
       
         <DelayDisplay />
         <ChangeOrderDisplay />
