98de0580e5d985b3979b29f2f6dc07c4c373bb28
Luke <Lukem>
2021-11-17 11:34:53 +1100

    disable ga

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import ReactGA from 'react-ga';
 
 function App() {
   const [mobile, setMobile] = useState(window.matchMedia("(max-width: 768px)").matches)
-  ReactGA.initialize('UA-210928692-2', {
-    debug: true,
-    titleCase: false,
-    gaOptions: {
-      userId: 123
-    }
-  });
+  // ReactGA.initialize('UA-210928692-2', {
+  //   debug: true,
+  //   titleCase: false,
+  //   gaOptions: {
+  //     userId: 123
+  //   }
+  // });
 
 
   return (
